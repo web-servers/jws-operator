ec154f75c5fa20d26e624a7ae905ed0e61226677
Jean-Frederic Clere <jfclere@gmail.com>
2020-08-30 11:10:14 +0200

    Arrange operator-sdk build.

--- build/setup-operator-sdk.sh
+++ build/setup-operator-sdk.sh
@@ -4,12 +4,14 @@ set -o nounset
 if ! [ -x "$(command -v operator-sdk)" ]; then
   echo 'Install operator-sdk' >&2
   CWD="$(pwd)"
+  rm -rf $GOPATH/src/github.com/operator-framework
   mkdir -p $GOPATH/src/github.com/operator-framework
   cd $GOPATH/src/github.com/operator-framework
   git clone https://github.com/operator-framework/operator-sdk
   cd operator-sdk
   git checkout master
-  make dep
+  export GOPROXY=proxy.golang.org
+  make tidy
   make install
   cd $CWD
 fi
