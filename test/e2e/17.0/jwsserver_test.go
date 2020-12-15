d39e344b961bdca045e4ae974c8fdc1d0aa3c2b7
Petros Marios Prokopiou <pprokopi@redhat.com>
2020-12-15 16:04:13 +0200

    Refactor test code

--- test/e2e/17.0/jwsserver_test.go
+++ test/e2e/17.0/jwsserver_test.go
@@ -7,26 +7,26 @@ import (
 
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/web-servers/jws-operator/pkg/apis"
-	jwsv1alpha1 "github.com/web-servers/jws-operator/pkg/apis/webservers/v1alpha1"
-	jwsframework "github.com/web-servers/jws-operator/test/framework"
+	webserversv1alpha1 "github.com/web-servers/jws-operator/pkg/apis/webservers/v1alpha1"
+	webserversframework "github.com/web-servers/jws-operator/test/framework"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestJWS17Server(t *testing.T) {
-	jwsServerList := &jwsv1alpha1.WebServerList{
+func TestWebServer54(t *testing.T) {
+	webServerList := &webserversv1alpha1.WebServerList{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "WebServer",
 			APIVersion: "web.servers.org/v1alpha1",
 		},
 	}
-	err := framework.AddToFrameworkScheme(apis.AddToScheme, jwsServerList)
+	err := framework.AddToFrameworkScheme(apis.AddToScheme, webServerList)
 	if err != nil {
 		t.Fatalf("failed to add custom resource scheme to framework: %v", err)
 	}
 	// run subtests
-	t.Run("BasicTest", jwsBasicTest)
+	t.Run("BasicTest", webServerBasicTest)
 }
 
-func jwsBasicTest(t *testing.T) {
-	jwsframework.JWSBasicTest(t, "17.0")
+func webServerBasicTest(t *testing.T) {
+	webserversframework.WebServerBasicTest(t, "54.0")
 }
