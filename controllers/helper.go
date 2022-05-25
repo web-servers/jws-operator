3c78eaa1c803f39bb557ba51d1250b923b796b0f
Jean-Frederic Clere <jfclere@gmail.com>
2022-05-25 16:04:24 +0200

    Fix issue #80, the operator was testing the view role in a loop.

--- controllers/helper.go
+++ controllers/helper.go
@@ -557,11 +557,14 @@ func (r *WebServerReconciler) setUseKUBEPing(webServer *webserversv1alpha1.WebSe
 	needUpdate := false
 	annotations := webServer.Annotations
 	if annotations == nil {
+		log.Info("The UseKUBEPing of WebServer is being updated: annotations == nil")
 		annotations = make(map[string]string)
 		needUpdate = true
 		annotations["UseKUBEPing"] = skubeping
 	} else {
+		log.Info("The UseKUBEPing of WebServer is being updated: annotations != nil")
 		if strings.Compare(skubeping, annotations["UseKUBEPing"]) != 0 {
+		        log.Info("The UseKUBEPing of WebServer is being updated: annotations != nil and changing!!!")
 			annotations["UseKUBEPing"] = skubeping
 			needUpdate = true
 		}
@@ -585,6 +588,13 @@ func (r *WebServerReconciler) getUseKUBEPing(webServer *webserversv1alpha1.WebSe
 				return false
 			}
 		}
+	}
+	return true
+}
+func (r *WebServerReconciler) needgetUseKUBEPing(webServer *webserversv1alpha1.WebServer) bool {
+	annotations := webServer.Annotations
+	if annotations != nil {
+		return false
 	}
 	return true
 }
