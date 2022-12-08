5f3e11ed897e09ce678ee3c131632c8dd51c9d38
Jean-Frederic Clere <jfclere@gmail.com>
2022-12-08 10:37:46 +0100

    Arrange install script.

--- test/scripts/nfs_install.sh
+++ test/scripts/nfs_install.sh
@@ -4,5 +4,8 @@ oc create -f nfs.yaml
 ip=`kubectl get services/nfs-service0 --output=jsonpath={..spec.clusterIP}`
 echo $ip
 sed s:1.2.3.4:$ip:g nfs-client-provisioner-tmp.yaml > nfs-client-provisioner.yaml
+sed s:1.2.3.4:$ip:g nfs-pv-tmp.yaml > nfs-pv.yaml
+sed s:1.2.3.4:$ip:g nfs-pv1-tmp.yaml > nfs-pv1.yaml
 oc create -f nfs-client-provisioner.yaml
 oc create -f nfs-pv.yaml
+oc create -f nfs-pv1.yaml
