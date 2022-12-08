390dab4cd7405adf29b5bd50806d99d515de9b8a
Jean-Frederic Clere <jfclere@gmail.com>
2022-12-08 11:11:23 +0100

    Add missing environment variable PROVISIONER_NAME and delete script.

---
+++ test/scripts/nfs_delete.sh
@@ -0,0 +1,6 @@
+kubectl delete deployment/nfs-client-provisioner
+kubectl delete deployment/my-nfs
+kubectl delete sc/nfs-client
+kubectl delete pv/pv0000
+kubectl delete pv/pv0001
+kubectl delete services/nfs-service0
