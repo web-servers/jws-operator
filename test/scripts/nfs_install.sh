330bc7e7eb5d996584ad7369124e4f50bbc315f2
Jean-Frederic Clere <jfclere@gmail.com>
2022-12-08 11:51:47 +0100

    Add missing storage class (it seems it works without it anyway).

--- test/scripts/nfs_install.sh
+++ test/scripts/nfs_install.sh
@@ -7,5 +7,6 @@ sed s:1.2.3.4:$ip:g nfs-client-provisioner-tmp.yaml > nfs-client-provisioner.yam
 sed s:1.2.3.4:$ip:g nfs-pv-tmp.yaml > nfs-pv.yaml
 sed s:1.2.3.4:$ip:g nfs-pv1-tmp.yaml > nfs-pv1.yaml
 oc create -f nfs-client-provisioner.yaml
+oc create -f nfs-client-sc.yaml
 oc create -f nfs-pv.yaml
 oc create -f nfs-pv1.yaml
