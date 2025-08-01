689caea2332398483b290477b2cb0589bc490fca
Matus Madzin <mmadzin@mmadzin-thinkpadx1carbongen11.remote.csb>
2025-08-01 12:41:05 +0200

    Draft: operator api update

--- api/v1alpha1/webserver_types.go
+++ api/v1alpha1/webserver_types.go
@@ -40,20 +40,20 @@ type WebServerSpec struct {
 	IsNotJWS bool `json:"isNotJWS,omitempty"`
 	// SecurityContext defines the security capabilities required to run the application.
 	SecurityContext *corev1.SecurityContext `json:"securityContext,omitempty"`
-        // Volume specification
+	// Volume specification
 	Volume *VolumeSpec `json:"volumeSpec,omitempty"`
 }
 
 // Volume specification
 type VolumeSpec struct {
-        // Persistence defines the PersistentVolumeClaimList specificaitons
-        PersistentVolumes []corev1.PersistentVolumeClaimSpec `json:"persistenceVolumes,omitempty"`
+	// PersistentVolumes define the PersistentVolumeClaimSpec specificaitons
+	PersistentVolumes []corev1.PersistentVolumeClaimSpec `json:"persistenceVolumes,omitempty"`
 	// SecretList
 	Secrets []string `json:"secrets,omitempty"`
 	// ConfigMapList
 	ConfigMaps []string `json:"configMaps,omitempty"`
-        // VolumeClaimTemplate for stateful applications
-        VolumeClaimTemplates []corev1.PersistentVolumeClaimSpec `json:"volumeClaimTemplates,omitempty"`
+	// VolumeClaimTemplate for stateful applications
+	VolumeClaimTemplates []corev1.PersistentVolumeClaimSpec `json:"volumeClaimTemplates,omitempty"`
 }
 
 // (Deployment method 1) Application image
