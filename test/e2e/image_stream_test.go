24dfd451bc2daf9cb6ca20a1afa47417df0c2d27
Matus Madzin <mmadzin@mmadzin-thinkpadx1carbongen11.remote.csb>
2025-08-11 17:56:13 +0200

    Image stream test

---
+++ test/e2e/image_stream_test.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/apimachinery/pkg/types"
+
+	webserversv1alpha1 "github.com/web-servers/jws-operator/api/v1alpha1"
+	"github.com/web-servers/jws-operator/test/utils"
+	//	kbappsv1 "k8s.io/api/apps/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("WebServer controller", Ordered, func() {
+	SetDefaultEventuallyTimeout(2 * time.Minute)
+	SetDefaultEventuallyPollingInterval(time.Second)
+
+	ctx := context.Background()
+	name := "app-img-stream-test"
+	appName := "image-stream-test"
+	namespace := "jws-operator-tests"
+	testURI := "/health"
+	imageStreamName := "jboss-webserver57-openjdk11-tomcat9-openshift-ubi8"
+	imageStreamNamespace := "openshift"
+
+	webserver := &webserversv1alpha1.WebServer{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "WebServer",
+			APIVersion: "web.servers.org/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: webserversv1alpha1.WebServerSpec{
+			Replicas:        2,
+			ApplicationName: appName,
+			WebImageStream: &webserversv1alpha1.WebImageStreamSpec{
+				ImageStreamName:      imageStreamName,
+				ImageStreamNamespace: imageStreamNamespace,
+			},
+		},
+	}
+
+	BeforeAll(func() {
+		// create the webserver
+		Expect(k8sClient.Create(ctx, webserver)).Should(Succeed())
+
+		// Check it is started.
+		webserverLookupKey := types.NamespacedName{Name: name, Namespace: namespace}
+		createdWebserver := &webserversv1alpha1.WebServer{}
+		Eventually(func() bool {
+			err := k8sClient.Get(ctx, webserverLookupKey, createdWebserver)
+			if err != nil {
+				return false
+			}
+			return true
+		}, time.Second*10, time.Millisecond*250).Should(BeTrue())
+		fmt.Printf("new WebServer Name: %s Namespace: %s\n", createdWebserver.ObjectMeta.Name, createdWebserver.ObjectMeta.Namespace)
+	})
+
+	AfterAll(func() {
+		k8sClient.Delete(context.Background(), webserver)
+		webserverLookupKey := types.NamespacedName{Name: name, Namespace: namespace}
+
+		Eventually(func() bool {
+			err := k8sClient.Get(ctx, webserverLookupKey, &webserversv1alpha1.WebServer{})
+			return apierrors.IsNotFound(err)
+		}, "2m", "5s").Should(BeTrue(), "the webserver should be deleted")
+	})
+
+	Context("ImageStreamSourceTest", func() {
+		It("Basic Test", func() {
+			_, err := utils.WebServerRouteTest(k8sClient, ctx, thetest, webserver, testURI, false, nil, false)
+			Expect(err).Should(Succeed())
+		})
+	})
+})
